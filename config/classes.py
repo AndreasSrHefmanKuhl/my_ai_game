84a049b6d821d7dc9d0f135f900998de17cdd827
heavsmac <hefbolzen@gmail.com>
2025-12-18 14:19:57 +0100

    created bullet class

---
+++ config/classes.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Bullet:
+    """Repräsentiert ein einzelnes Projektil."""
+
+    def __init__(self, x, y, direction):
+        # Erstellt ein kleines Rechteck für die Kugel
+        self.rect = pygame.Rect(x, y, 12, 4)
+        self.color = (255, 200, 0)  # Gold/Gelb
+        self.speed = 500  # Pixel pro Sekunde
+        self.direction = direction  # 1 für rechts, -1 für links
+
+    def update(self, dt):
+        """Bewegt die Kugel basierend auf der Zeit."""
+        self.rect.x += self.speed * dt * self.direction
+
+    def draw(self, surface):
+        """Zeichnet die Kugel auf das Display."""
+        pygame.draw.rect(surface, self.color, self.rect)
