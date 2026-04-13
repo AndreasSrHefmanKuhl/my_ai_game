1554c1947187be5533374840fa6b2984ffca0e52
heavsmac <hefbolzen@gmail.com>
2026-04-13 15:55:41 +0200

    fixed agent role and return in short

--- user_interface/agent.py
+++ user_interface/agent.py
@@ -1,25 +1,19 @@
 from typing import Annotated, Sequence, TypedDict
 import operator
 import os
-
 from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode, tools_condition
-
 load_dotenv()
 
 # Ensure keys are set in your .env file
 OPENAI_API_KEY = os.getenv("OAI_KEY")
-"""TO DO (hier kommen die tool für den agent hin, damit er weiß später wie zu reagieren(funkti0nen sind erwünscht))"""
-#TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+"""TO DO (hier kommen die tool für den agent hin, damit er weiß später wie zu reagieren(funktionen sind erwünscht))"""
 
-#  Setup Tools
-#tavily_tool = TavilySearchResults(max_results=5)
-#tools = [tavily_tool]
-
+tools =[]
 #  Define State
 class AgentState(TypedDict):
     #  appends new messages to the existing list
@@ -27,15 +21,23 @@ class AgentState(TypedDict):
 
 #  Setup Model
 model = ChatOpenAI(
-    temperature=0,
-    model="gpt-3.5-turbo",
+    api_key=OPENAI_API_KEY,
+    temperature=0.3,
+    model= "gpt-4o-mini",
     streaming=True
 ).bind_tools(tools)
 
 #  Define Nodes
 def call_model(state: AgentState):
+    # System-Anweisung hinzufügen
+    system_message = {
+        "role": "system",
+        "content": "You are an API. answer ONLY with a python-list, NO text,NO explanation"
+    }
+    messages = [system_message] + list(state["messages"])
+
     print("--- Calling Model ---")
-    response = model.invoke(state["messages"])
+    response = model.invoke(messages)
     return {"messages": [response]}
 
 #  Build Graph
